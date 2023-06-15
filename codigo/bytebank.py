bc916a5f55615979315132410174723dd83a7ec2
Lucas Nobre Barbosa <lucasbnobre18@gmail.com>
2023-06-15 09:17:59 -0300

    Add new tests following TDD

--- codigo/bytebank.py
+++ codigo/bytebank.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+
 class Funcionario:
     def __init__(self, nome, data_nascimento, salario):
         self._nome = nome
@@ -19,7 +20,7 @@ class Funcionario:
         ano_nascimento = data_nascimento[-1]
         ano_atual = date.today().year
         return ano_atual - int(ano_nascimento)
-    
+
     def sobrenome(self):
         nome_completo = self.nome.strip()
         nome_dividido = nome_completo.split(' ')
@@ -28,8 +29,18 @@ class Funcionario:
     def calcular_bonus(self):
         valor = self._salario * 0.1
         if valor > 1000:
-            valor = 0
+            raise Exception("O salário é muito alto para receber um bônus")
         return valor
+
+    def _eh_socio(self):
+        sobrenomes_socios = ['Bragança', 'Windsor', 'Bourbon',
+                             'Yamato', 'Al Saud', 'Khan', 'Tudor', 'Ptolomeu']
+        return self._salario >= 100000 and self.sobrenome() in sobrenomes_socios
+
+    def decrescimo_salario(self):
+        if self._eh_socio():
+            decrescimo = self._salario * 0.1
+            self._salario = self._salario - decrescimo
 
     def __str__(self):
         return f'Funcionario({self._nome}, {self._data_nascimento}, {self._salario})'
--- tests/test_funcionario.py
+++ tests/test_funcionario.py
@@ -5,17 +5,38 @@ class TestFuncionario:
     def test_quando_idade_recebe_01_01_2001_deve_retornar_ano_atual_menos_2001(self):
         entrada_dada = '01/01/2001'
         saida_esperada = 22
-        
+
         funcionario = Funcionario('Teste', entrada_dada, 1111)
         resultado_obtido = funcionario.idade()
-        
+
         assert resultado_obtido == saida_esperada
-        
+
     def test_quando_sobrenome_recebe_Lucas_Nobre_deve_retornar_Nobre(self):
         entrada_dada = 'Lucas Nobre'
         saida_esperada = 'Nobre'
-        
+
         lucas = Funcionario(entrada_dada, '11/11/2001', 1111)
         resultado_obtido = lucas.sobrenome()
+
+        assert resultado_obtido == saida_esperada
+
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada_dada = 100000
+        saida_esperada = 90000
+
+        funcionario_diretor = Funcionario(
+            'Paulo Bragança', '11/11/2000', entrada_dada)
+        funcionario_diretor.decrescimo_salario()
+
+        resultado_obtido = funcionario_diretor.salario
+
+        assert resultado_obtido == saida_esperada
+
+    def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
+        entrada_dada = 1000
+        saida_esperada = 100
+        
+        funcionario = Funcionario('Teste', '11/11/2000', entrada_dada)
+        resultado_obtido = funcionario.calcular_bonus()
         
         assert resultado_obtido == saida_esperada
